4ac051b628399c49b40af07acc9c08dc9ffda845
1jnolan <81715638+1jnolan@users.noreply.github.com>
2026-01-26 00:19:46 +0000

    Create U03.10.04 Analytics T4p84.py

---
+++ U03.10.04 Analytics T4p84.py
@@ -0,0 +1,14 @@
+"""
+File name: U03.10.04 Analytics T4p84.py
+#*******************************************************
+#Enter your Name:
+#*******************************************************
+Task 4 Page 84
+***************
+Run a text analysis on a book of your choice that is a text file
+Open the file, and split it with a delimeter of spaces, then conduct frequency
+and mode analysis on the list of words.
+
+Display the top 20 words (in order of frequency)
+And display the mode set for this set of 20 words
+"""
