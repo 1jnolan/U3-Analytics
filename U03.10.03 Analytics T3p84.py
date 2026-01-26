a7c902a9345001087bba203acdbcdcb997b37712
1jnolan <81715638+1jnolan@users.noreply.github.com>
2026-01-26 00:14:54 +0000

    Create U03.10.03 Analytics T3p84.py

---
+++ U03.10.03 Analytics T3p84.py
@@ -0,0 +1,12 @@
+"""
+File name: U03.10.03 Analytics T3p84.py
+#*******************************************************
+#Enter your Name:
+#*******************************************************
+Task 3 Page 84
+***************
+Search on the lottery website and find the past month's lottery numbers for the irish lottery
+
+Enter them manually onto a list
+Calculate the frequency of each number
+Determine if the distribution is spead evenly over all numbers"""
