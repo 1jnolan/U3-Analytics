510cc642ac9bc6ca2d975f48effada85ec2a3134
1jnolan <81715638+1jnolan@users.noreply.github.com>
2026-01-26 00:07:54 +0000

    Create U03.01 Analytics T1p84.py

---
+++ U03.01 Analytics T1p84.py
@@ -0,0 +1,11 @@
+"""
+File name: U03.01 Analytics T1p84.py
+#*******************************************************
+#Enter your Name:
+#*******************************************************
+Task 1 Page 84
+***************
+Write the code to calculate the minimum, maximum and mean values of these lists:"""
+ages=[32, 31, 30, 37, 37, 33] 
+
+earnings= [1270, 7980, 4700, 1810, 750, 1100]
