5b2c6ef3de73128c8f988d0993c5e4faf263454e
1jnolan <81715638+1jnolan@users.noreply.github.com>
2026-01-26 00:11:36 +0000

    Create U03.10.02 Analytics T2p84.py

---
+++ U03.10.02 Analytics T2p84.py
@@ -0,0 +1,11 @@
+"""
+File name: U03.10.02 Analytics T2p84.py
+#*******************************************************
+#Enter your Name:
+#*******************************************************
+Task 2 Page 84
+***************
+For the list [1,9,3,2,3,7,6,5,8,9,1,10]
+a. calculate the minimum and maximum values
+b. mean, median, frequency and mode"""
+ 
