2e0de4fb7128d8283fab295dabf8407717a8c0c9
1jnolan <81715638+1jnolan@users.noreply.github.com>
2026-02-25 12:14:37 +0000

    Create U03.08.02 Mode using statistics.py

---
+++ U03.08.02 Mode using statistics.py
@@ -0,0 +1,7 @@
+import statistics
+#a list of all of the entries
+myList=['red', 'blue', 'green', 'red', 'blue', 'red', 'blue', 'red','green', 'red']
+
+#a variable to store the result
+mode=statistics.mode(myList)
+print(mode)
