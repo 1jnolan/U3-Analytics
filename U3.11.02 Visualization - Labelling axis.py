aacc88c9cbafcee8ad37f5a5cd96dc565379b9ad
1jnolan <81715638+1jnolan@users.noreply.github.com>
2026-02-26 11:55:52 +0000

    Create U3.11.02 Visualization - Labelling axis.py

---
+++ U3.11.02 Visualization - Labelling axis.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+numBS=[1,2.5,4,3,2.5,2] #a list with the data
+plt.plot(numBS) #Send this list to plt to plot it.
+
+plt.title("Brothers and Sisters") #Add your title
+plt.xlabel("List Index") #Add the x label
+plt.ylabel("Number of Brothers & Sisters") #Add the y label
+plt.show() #plot the graph
