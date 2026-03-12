9e99faaa57f7b50272a2128bc6a521fc46b01499
1jnolan <81715638+1jnolan@users.noreply.github.com>
2026-03-12 11:40:57 +0000

    Create U03.13.06 Eurovision.py

---
+++ U03.13.06 Eurovision.py
@@ -0,0 +1,13 @@
+#Pygal Embedding images - Eurovision
+
+# Total wins by country
+countries = ["Ireland", "Sweden", "France", "Luxembourg", "Netherlands", "United Kingdom", "Israel"]
+total_wins = [7, 7, 5, 5, 5, 5, 4]
+
+# Nul points received
+nul_countries = ["Austria", "Norway", "Finland", "Spain", "Switzerland", "Germany"]
+nul_points = [4, 4, 3, 3, 3, 3]
+
+# Last-place finishes
+last_place_countries = ["Norway", "Finland", "Germany", "Austria"]
+last_place_finishes = [12, 9, 9, 7]
